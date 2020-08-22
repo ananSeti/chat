5c5894cc17aa0863e23b9e06537f8381b5951f2c
anan <anan.sati@gmail.com>
2020-08-22 07:25:01 +0700

    modify infomation all

--- auth.py
+++ auth.py
@@ -21,28 +21,29 @@ def register():
         cur =dbConn.cursor()
 
         error =None
-        rows = None
+        #rows = None
         #print(error)
 
-        cur.execute("SELECT id FROM a_user WHERE username = %s", (username,))
-        rows =cur.fetchall() 
-        for row in rows:
+        #cur.execute("SELECT id FROM a_user WHERE username = %s", (username,))
+        #rows =cur.fetchall() 
+        #for row in rows:
             #print(row[0]) 
-            error ='User {} is already registered'.format(username)
+        #    error ='User {} is already registered'.format(username)
         if not username:
             error ='Username is required.'
         elif not password:
             error ='Password is required.'
-        elif(
-             #db.execute("SELECT id FROM a_user WHERE username = %s", (username,)
-             cur.execute("SELECT id FROM a_user WHERE username = %s", (username,))
-             #cur.fetchone()  #is not None:
-             #row =cur.fetchone()
+        else:
+            cur.execute("SELECT id FROM a_user WHERE username = %s", (username,))
+            row =cur.fetchone()
              #rows = cur.fetchall() 
-              #  for row in rows:  
+             #for row in rows:  
               #  error = 'User {} is already registered'.format(username)
-            ):
-            error = 'Usersss {} is already registered'.format(username)
+            
+            if row is not None:  
+                 error = 'User {} is already registered'.format(username)
+                 error =error +" id is :" + str(row[0])
+                 #error = 'Usersss {} is already registered'.format(row)
                                    
 
             #error =f'User {username} is already registered.'
@@ -51,7 +52,7 @@ def register():
             #the login page
             #cur = db.cursor()
             #db.execute(
-            print(username)   
+               
             cur.execute(   
             "INSERT INTO a_user (username, password) VALUES (%s, %s)",
                 (username, generate_password_hash(password))
@@ -63,6 +64,31 @@ def register():
         flash(error)
     return render_template('auth/register.html')
 
+
+@bp.route('/login',methods=('GET','POST'))
+def login():
+        if request.method =='POST':
+            username = request.form['username']
+            password = request.form['password']
+            dbConn =get_db()
+            cur =dbConn.cursor()
+            error =None
+            cur.execute('SELECT * FROM a_user WHERE username = %s', (username,))
+            user =cur.fetchone()
+            
+            if user is None:
+                error ='Incorrect username'
+                                         #user['password']       
+            elif not check_password_hash(user[2],password):
+                error ='Incorrenct password'
+            #else:
+            #   error =  str(user[2])
+            if error is None:
+                session.clear()
+                session['user_id'] = user[0]  #userID
+                return redirect(url_for('auth.info')) #chang index to info
+            flash(error)
+        return render_template('auth/login.html')
 @bp.route('/info',methods=('GET','POST'))
 def info():
     if request.method =='POST':
@@ -90,28 +116,10 @@ def info():
 
     return render_template('auth/info.html')
 
-@bp.route('/login',methods=('GET','POST'))
-def login():
-        if request.method =='POST':
-            username = request.form['username']
-            password = request.form['password']
-            db =get_db()
-            error =None
-            user = db.execute('SELECT * FROM a_user WHERE username = %s', (username,)
-            )
-            #user =users.fetchone()
-            
-            if user is None:
-                error ='Incorrect username'
-            elif not check_password_hash(user['password'],password):
-                error ='Incorrenct password'
-            
-            if error is None:
-                session.clear()
-                session['user_id'] = user['id']
-                return redirect(url_for('index'))
-            flash(error)
-        return render_template('auth/login.html')
 @bp.route('/test',methods=('GET','POST'))
 def test():
     return "test"
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('test'))
